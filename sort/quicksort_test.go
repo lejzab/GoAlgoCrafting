0c94f327913c74a64c320fb3fc304b8af8a64527
Paweł Bogaczewicz <0x645@tutanota.com>
2023-08-20 22:54:37 +0200

    adding test for empty and single element slice

--- sort/quicksort_test.go
+++ sort/quicksort_test.go
@@ -12,7 +12,10 @@ func TestQuickSort(t *testing.T) {
 	}{
 		{[]int{7, 2, 1, 6, 8, 5, 3, 4}, []int{1, 2, 3, 4, 5, 6, 7, 8}},
 		{[]int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}, []int{1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9}},
+		{[]int{-1}, []int{-1}},
+		{[]int{}, []int{}},
 		// Add more test cases here
+
 	}
 
 	for _, test := range tests {
