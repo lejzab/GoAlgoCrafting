00fb6707ca46e037a5db32b96ae7ee29cfe92272
Paweł Bogaczewicz <0x645@tutanota.com>
2023-08-31 15:18:20 +0200

    dodane nazwy testów

--- sort/mergesort_test.go
+++ sort/mergesort_test.go
@@ -7,23 +7,37 @@ import (
 
 func TestMergeSort(t *testing.T) {
 	tests := []struct {
-		input []int
-		want  []int
+		name     string
+		input    []int
+		expected []int
 	}{
-		{[]int{7, 2, 1, 6, 8}, []int{1, 2, 6, 7, 8}},
-		{[]int{7, 2, 1, 6, 8, 5, 3, 4}, []int{1, 2, 3, 4, 5, 6, 7, 8}},
-		{[]int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}, []int{1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9}},
-		{[]int{-1}, []int{-1}},
-		{[]int{}, []int{}},
+		{name: "Simple sort",
+			input:    []int{7, 2, 1, 6, 8, 5, 3, 4},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8},
+		},
+		{
+			name:     "Double elements",
+			input:    []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5},
+			expected: []int{1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9},
+		},
+		{
+			name:     "Single element",
+			input:    []int{-1},
+			expected: []int{-1},
+		},
+		{
+			name:     "Empty array",
+			input:    []int{},
+			expected: []int{},
+		},
 		// Add more test cases here
-
 	}
 
 	for _, test := range tests {
-		t.Run("", func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {
 			result := MergeSort(test.input)
-			if !reflect.DeepEqual(result, test.want) {
-				t.Errorf("got %v, want %v", test.input, test.want)
+			if !reflect.DeepEqual(result, test.expected) {
+				t.Errorf("got %v, expected %v", test.input, test.expected)
 			}
 		})
 	}
